7539f905df5f2cefd4faa10763751acfe9975d7f
wang.puyu <wang.puyu@synyi.com>
2021-07-05 16:42:59 +0800

    示例代码

---
+++ SimpleWebServer.Web/CallbackThread.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+
+namespace SimpleWebServer.Web
+{
+    public class CallbackThread
+    {
+        private static readonly Lazy<CallbackThread> Lazy =
+            new Lazy<CallbackThread>(() => new CallbackThread());
+
+        public static CallbackThread Instance => Lazy.Value;
+        
+        private CallbackThread()
+        {
+            Execute();
+        }
+
+        private readonly SortedList<DateTime, LinkedList<Action>> _waitCallbackList =
+            new SortedList<DateTime, LinkedList<Action>>(200);
+
+        private readonly Random _random = new Random();
+
+        private bool _stopped = true;
+
+        private static readonly object LockObj = new object();
+        public void Register(Action callback)
+        {
+            lock (LockObj)
+            {
+                var dateTime = DateTime.Now.AddMilliseconds(_random.Next(1000, 2000));
+                if (_waitCallbackList.ContainsKey(dateTime))
+                {
+                    _waitCallbackList[dateTime].AddLast(callback);
+                }
+                else
+                {
+                    _waitCallbackList.Add(dateTime, new LinkedList<Action>(new[] {callback}));
+                }
+            }
+        }
+
+        public void Execute()
+        {
+            if (_stopped)
+            {
+                _stopped = false;
+                var thread = new Thread((() =>
+                {
+                    while (!_stopped)
+                    {
+                        lock (LockObj)
+                        {
+                            if (_waitCallbackList.Count > 0)
+                            {
+                                if (_waitCallbackList.Keys[0] <= DateTime.Now)
+                                {
+                                    var actions = _waitCallbackList[_waitCallbackList.Keys[0]];
+                                    foreach (var action in actions)
+                                    {
+                                        action();
+                                    }
+                                    _waitCallbackList.Remove(_waitCallbackList.Keys[0]);
+                                }
+                            }
+                        }
+                        Thread.Sleep(0);
+                    }
+                }))
+                {
+                    IsBackground = true
+                };
+                thread.Start();
+            }
+        }
+    }
+}
---
+++ SimpleWebServer.Web/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace SimpleWebServer.Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://127.0.0.1:5000/");
+            listener.Start();
+            //SyncListener(listener);
+            //SyncListenerWithThread(listener);
+            AsyncListener(listener);
+        }
+
+        private static readonly Random Random = new Random();
+
+        /// <summary>
+        /// 单线程
+        /// </summary>
+        /// <param name="listener"></param>
+        static void SyncListener(HttpListener listener)
+        {
+            while (true)
+            {
+                //接收请求
+                var context = listener.GetContext();
+                Thread.Sleep(Random.Next(1000, 2000));
+                SetResponse(context);
+                Thread.Sleep(0);
+            }
+        }
+
+        /// <summary>
+        /// 多线程
+        /// </summary>
+        /// <param name="listener"></param>
+        static void SyncListenerWithThread(HttpListener listener)
+        {
+            while (true)
+            {
+                //接收请求
+                var context = listener.GetContext();
+                var thread = new Thread(() =>
+                {
+                    Thread.Sleep(Random.Next(1000, 2000));
+                    SetResponse(context);
+                });
+                thread.Start();
+                Thread.Sleep(0);
+            }
+        }
+
+        /// <summary>
+        /// 异步模拟
+        /// </summary>
+        /// <param name="listener"></param>
+        static void AsyncListener(HttpListener listener)
+        {
+            while (true)
+            {
+                //等待传入请求时，此方法会阻塞线程
+                var context = listener.GetContext();
+                CallbackThread.Instance.Register(() =>
+                {
+                    SetResponse(context);
+                });
+                Thread.Sleep(0);
+            }
+        }
+
+        private static void SetResponse(HttpListenerContext context)
+        {
+            var response = context.Response;
+            var responseString = "Hello world!";
+            var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            var output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
+        }
+    }
+}
