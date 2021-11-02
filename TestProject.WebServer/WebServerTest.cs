0159c4fdf2b73390e657661b95f627e31d5591ec
wang.puyu <wang.puyu@synyi.com>
2021-11-02 18:21:00 +0800

    示例更新

--- TestProject.WebServer/WebServerTest.cs
+++ TestProject.WebServer/WebServerTest.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections;
+using System.Diagnostics;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Castle.DynamicProxy;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Xunit;
 using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace TestProject.WebServer
 {
@@ -154,24 +158,25 @@ namespace TestProject.WebServer
         [Fact(DisplayName = "使用await Task进行异步IO")]
         public void Start7()
         {
+            async void GetContentType(HttpListenerContext context)
+            {
+                var req = WebRequest.Create("http://www.baidu.com");
+                req.Method = "GET";
+                var res = await req.GetResponseAsync();
+                SetResponse(context, res.ContentType);
+            }
             while (true)
             {
                 //接收请求
                 var context = _listener.GetContext();
-                async void GetContentType()
-                {
-                    var req = WebRequest.Create("http://www.baidu.com");
-                    req.Method = "GET";
-                    var res = await req.GetResponseAsync();
-                    SetResponse(context, res.ContentType);
-                }
-                GetContentType();
+                GetContentType(context);
             }
         }
 
         [Fact(DisplayName = ".Net 1.x APM模式的异步")]
         public void Test1()
         {
+            //逻辑是分离的，需要花费大量的精力，为一小部分代码维护一大段上下文，在回调函数中再通过危险的Cast操作恢复数据
             var req = WebRequest.Create("http://www.baidu.com");
             req.Method = "GET";
             req.BeginGetResponse(EndGetResponse, new SendResponseState { Context = null, WebRequest = req });
@@ -197,6 +202,7 @@ namespace TestProject.WebServer
         [Fact(DisplayName = ".Net 2.x APM模式的异步")]
         public void Test2()
         {
+            //最重要的进化，可以闭包捕获上下文，即使逻辑比较复杂，和主方法分离，也可以利用捕获的强类型变量，让分离的逻辑更可读
             var req = WebRequest.Create("http://www.baidu.com");
             req.Method = "GET";
             req.BeginGetResponse(delegate (IAsyncResult ar)
@@ -212,6 +218,7 @@ namespace TestProject.WebServer
         [Fact(DisplayName = ".Net 3.x APM模式的异步")]
         public void Test3()
         {
+            //小提升，简化了匿名委托的方法，不需要再指定委托参数的类型
             var req = WebRequest.Create("http://www.baidu.com");
             req.Method = "GET";
             req.BeginGetResponse(ar =>
@@ -227,6 +234,7 @@ namespace TestProject.WebServer
         [Fact(DisplayName = ".Net 4.x TAP模式的异步")]
         public void Test4()
         {
+            //进一步简化，从BeginXX和EndXX两个方法简化为一个方法，提供了统一的中断任务的方式
             var req = WebRequest.Create("http://www.baidu.com");
             req.Method = "GET";
             var task = req.GetResponseAsync().ContinueWith(r =>
@@ -241,6 +249,7 @@ namespace TestProject.WebServer
         [Fact(DisplayName = ".Net 4.x TAP模式的异步")]
         public async void Test5()
         {
+            //解决了回调嵌套问题、异常捕获等问题，使用同步的思维，来解决异步的问题。
             var req = WebRequest.Create("http://www.baidu.com");
             req.Method = "GET";
             var task = req.GetResponseAsync();
@@ -265,6 +274,14 @@ namespace TestProject.WebServer
             task.Wait();
         }
 
+        [Fact(DisplayName = "如何动态代理异步方法")]
+        public async Task Test7()
+        {
+            var testClass = new ProxyGenerator().CreateClassProxy<AsyncTestClass>(new Interceptor());
+            var res = await testClass.Get();
+            Assert.Equal(2, res);
+        }
+
         private Task<WebResponse> SelfGetResponseAsync(WebRequest webRequest)
         {
             var taskSource = new TaskCompletionSource<WebResponse>();
@@ -273,6 +290,47 @@ namespace TestProject.WebServer
                 taskSource.SetResult(webRequest.EndGetResponse(ar));
             }, webRequest);
             return taskSource.Task;
+        }
+    }
+
+    public class AsyncTestClass
+    {
+        public virtual async Task<int> Get()
+        {
+            Debug.WriteLine("实例方法执行前");
+            await Task.Delay(1000);
+            Debug.WriteLine("实例方法执行后");
+            return 1;
+        }
+    }
+
+    public class Interceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            Debug.WriteLine("代理方法执行前");
+            invocation.Proceed();
+            var task = (Task<int>)invocation.ReturnValue;
+            invocation.ReturnValue = Test(task);
+            Debug.WriteLine("代理方法执行后");
+        }
+
+        async Task<int> DoAfter(Task<int> task)
+        {
+            var res = await task;
+            Debug.WriteLine("异步方法执行后");
+            return res + 1;
+        }
+
+        private Task<int> Test(Task<int> oldTask)
+        {
+            var task = new TaskCompletionSource<int>();
+            oldTask.ContinueWith(r =>
+            {
+                var res = r.Result;
+                task.SetResult(res + 1);
+            });
+            return task.Task;
         }
     }
 }
